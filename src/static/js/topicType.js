9413dadb3b56d95a2d5cae20a70916434d34a0fc
wanggenmiao <1156916870@qq.com>
2022-06-09 16:19:21 +0800

    首页侧边栏js文件

---
+++ src/static/js/topicType.js
@@ -0,0 +1,38 @@
+const topicType = [
+  {
+    value: 'all',
+    label: '全部',
+  },
+  {
+    value: 'good',
+    label: '精华',
+    color: '#80bd01'
+  },
+  {
+    value: 'share',
+    label: '分享',
+    color: 'purple'
+  },
+  {
+    value: 'ask',
+    label: '问答',
+    color: 'geekblue'
+  },
+  {
+    value: 'job',
+    label: '招聘',
+    color: 'gold'
+  },
+  {
+    value: 'dev',
+    label: '测试',
+    color: '#999'
+  },
+  {
+    value: 'top',
+    label: '置顶',
+    color: 'green'
+  }
+]
+
+export default topicType
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,26 +1,20 @@
 import { Fragment, useState } from "react"
 import { Link, Outlet } from "react-router-dom"
 import Footer from "../../components/footer/Footer"
+import topicType from "../../static/js/topicType"
 import './home.scss'
 function Home () {
-  const leftNav = [
-    { label: '全部', path: 'all' },
-    { label: '精华', path: 'good' },
-    { label: '分享', path: 'share' },
-    { label: '问答', path: 'talk' },
-    { label: '招聘', path: 'recruit' },
-    { label: '测试', path: 'test' },
-  ]
+  const leftNav = topicType
   const [acvtiveNav, setAcvtiveNav] = useState('all')
   return (
     <Fragment>
       <div className="home">
         <div className="leftNav" >
           {leftNav.map(nav => <Link
-            className={[acvtiveNav === nav.path ? 'active' : ''].join(' ')}
-            key={nav.path}
-            to={`/home/${nav.path}`}
-            onClick={() => setAcvtiveNav(nav.path)}
+            className={[acvtiveNav === nav.value ? 'active' : ''].join(' ')}
+            key={nav.value}
+            to={`/home/${nav.value}`}
+            onClick={() => setAcvtiveNav(nav.value)}
           >{nav.label}</Link>)}
         </div>
         <div className='right'>
