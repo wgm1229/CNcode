f1d225af365c3a2f0102b0018ba24419dcab5cdb
wanggenmiao <1156916870@qq.com>
2022-06-20 17:54:56 +0800

    教程；重复样式整理

--- src/views/course/Course.jsx
+++ src/views/course/Course.jsx
@@ -1,6 +1,19 @@
+import { Card } from 'antd'
+import courseData from './courseData'
+import Footer from '../../components/footer/Footer'
+import './course.scss'
+import { Fragment } from 'react'
 function Course () {
   return (
-    <div>course</div>
+    <Fragment>
+      <div className="course">
+        {courseData.map(card => <Card key={card.title} title={card.title} >
+          <div dangerouslySetInnerHTML={{ __html: card.content }}></div>
+        </Card>
+        )}
+      </div>
+      <Footer />
+    </Fragment>
   )
 }
 export default Course
