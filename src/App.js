c49f718d7a50086d4f4674623cc607b546299f79
wanggenmiao <1156916870@qq.com>
2022-06-20 10:45:43 +0800

    首页重定向

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  useNavigate,
 } from "react-router-dom";
+import { useEffect } from "react";
 import HomeBox from "./views/home/Home";
 import Top from "./views/TopNav/TopNav.jsx";
 import About from './views/about/About.jsx'
@@ -10,12 +12,19 @@ import Course from './views/course/Course.jsx'
 import SimplifyList from "./views/list/List";
 import Article from "./views/home/article/Article";
 import User from "./views/user/User";
+function Redirect ({ to }) {
+  let navigate = useNavigate()
+  useEffect(() => {
+    navigate(to)
+  })
+  return null
+}
 function App () {
   return (
     <BrowserRouter>
       <Top />
       <Routes>
-        <Route path="/" element={<HomeBox />}></Route>
+        <Route path="/" element={<Redirect to={'/home/all'} />}></Route>
         <Route path="/home" element={<HomeBox />}>
           <Route index path=":type" element={<SimplifyList />} />
         </Route>
