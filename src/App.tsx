1777cdda4d6019463488ac83767f05d062339ff1
wanggenmiao <1156916870@qq.com>
2022-07-22 14:13:42 +0800

    首页左侧菜单

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 import Home from './views/home/Home'
 import TopNav from './views/top/TopNav'
 import Footer from './views/footer/Footer'
 import Course from './views/course/Course'
 import About from './views/about/About'
+type path = {
+  to: string
+}
+function Redirect(param: path) {
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate(param.to)
+  })
+  return null
+}
 function App() {
   return (
     <BrowserRouter>
       <TopNav />
       <main>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Redirect to={'/home'} />}></Route>
           <Route path="/home" element={<Home />}></Route>
           <Route path="/Course" element={<Course />}></Route>
           <Route path="/About" element={<About />}></Route>
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,4 +1,54 @@
+import './home.scss'
+import { useState } from 'react'
+type nav = {
+  label: string
+  value: string
+  // active?: boolean
+}
 function Home() {
-  return <div>home</div>
+  const leftNav: Array<nav> = [
+    {
+      label: '全部',
+      value: 'all'
+    },
+    {
+      label: '精华',
+      value: 'good'
+    },
+    {
+      label: '分享',
+      value: 'share'
+    },
+    {
+      label: '问答',
+      value: 'ask'
+    },
+    {
+      label: '招聘',
+      value: 'gold'
+    },
+    {
+      label: '测试',
+      value: 'test'
+    }
+  ]
+  const [currNav, setCurrNav] = useState<string>('all')
+  const changeNav = (nav: nav) => {
+    setCurrNav(nav.value)
+  }
+  return (
+    <div className="home">
+      <div className="left">
+        {leftNav.map((nav) => {
+          return (
+            <span className={currNav === nav.value ? 'active' : ''} key={nav.value} onClick={() => changeNav(nav)}>
+              {nav.label}
+            </span>
+          )
+        })}
+      </div>
+      <div className="right">right</div>
+    </div>
+  )
 }
 export default Home
