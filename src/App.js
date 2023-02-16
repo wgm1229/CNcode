b359e5f6b3fa595c0c90928f522cbf82be648042
wanggenmiao <1156916870@qq.com>
2023-02-16 15:10:21 +0800

    antd、react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import './App.css'
-function App () {
-  return (
-    <div className="App">
-      react start
-    </div>
-  );
+function App() {
+  return <div className="App">react start</div>;
 }
 
 export default App;
